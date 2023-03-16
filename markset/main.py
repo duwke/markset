3762cfcc33ce0d7c016c90b766ff157dd01e4e5d
Darin Keever <dkeever@nauticusrobotics.com>
2023-03-16 09:53:31 -0500

    huh

--- markset/main.py
+++ markset/main.py
@@ -123,7 +123,7 @@ async def race_api(mode):
 async def restart_webserver():
     print("Restart")
     with open("main.py", "a") as a_file:
-        a_file.write("\n")
+        a_file.write("   #test\n")
 
 @app.route('/api/computer/<command>', methods=['POST'])
 async def computer_control_api(command):
